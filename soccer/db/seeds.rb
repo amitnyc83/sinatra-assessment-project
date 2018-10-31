9d7bcab2287490fc0b1fcbeb7476910a95439bfe
amitnyc83 <amit_nyc@live.com>
2018-10-31 12:28:40 -0400

    rename seed file

---
+++ soccer/db/seeds.rb
@@ -0,0 +1,13 @@
+owner1 = Owner.create!(:name => 'Amit', :username => 'amit', :password => '1234', :team_id => team1.id)
+owner2 = Owner.create!(:name => 'Jack', :username => 'jack', :password => '12345', :team_id => team2.id)
+owner3 = Owner.create!(:name => 'Reno', :username => 'reno', :password => '123456', :team_id => team3.id)
+
+team1 = Team.create!(:name => 'Liverpool', :owner_id => owner1.id, :player_id => player1.id)
+team2 = Team.create!(:name => 'Chelsea', :owner_id => owner2.id, :player_id => player2.id)
+team3 = Team.create!(:name => 'Juventus', :owner_id => owner3.id, :player_id => player3.id)
+
+
+
+player1 = Player.create!(:name => 'Gerrard', :position => 'Midfield', :value => '20', :team_id => team1.id)
+player2 = Player.create!(:name => 'Hazard', :position => 'Attacking Midfield', :value => '40', :team_id => team2.id)
+player3 = Player.create!(:name => 'Ronaldo', :position => 'Striker', :value => '100', :team_id => team3.id)
