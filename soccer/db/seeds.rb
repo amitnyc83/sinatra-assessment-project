de579b27a8daa6c45ce409235e1448300d712b73
amitnyc83 <amit_nyc@live.com>
2018-10-31 13:21:50 -0400

    seed db

--- soccer/db/seeds.rb
+++ soccer/db/seeds.rb
@@ -1,10 +1,10 @@
-owner1 = Owner.create!(:name => 'Amit', :username => 'amit', :password => '1234', :team_id => team1.id)
-owner2 = Owner.create!(:name => 'Jack', :username => 'jack', :password => '12345', :team_id => team2.id)
-owner3 = Owner.create!(:name => 'Reno', :username => 'reno', :password => '123456', :team_id => team3.id)
+owner1 = Owner.create!(:name => 'Amit', :username => 'amit', :password => '1234')
+owner2 = Owner.create!(:name => 'Jack', :username => 'jack', :password => '12345')
+owner3 = Owner.create!(:name => 'Reno', :username => 'reno', :password => '123456')
 
-team1 = Team.create!(:name => 'Liverpool', :owner_id => owner1.id, :player_id => player1.id)
-team2 = Team.create!(:name => 'Chelsea', :owner_id => owner2.id, :player_id => player2.id)
-team3 = Team.create!(:name => 'Juventus', :owner_id => owner3.id, :player_id => player3.id)
+team1 = Team.create!(:name => 'Liverpool', :owner_id => owner1.id)
+team2 = Team.create!(:name => 'Chelsea', :owner_id => owner2.id)
+team3 = Team.create!(:name => 'Juventus', :owner_id => owner3.id)
 
 
 
