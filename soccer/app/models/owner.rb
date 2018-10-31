79642a69305c7e9108bfd9de10c07fc0df719d29
amitnyc83 <amit_nyc@live.com>
2018-10-31 12:27:50 -0400

    make associations between models

--- soccer/app/models/owner.rb
+++ soccer/app/models/owner.rb
@@ -1,2 +1,17 @@
 class Owner < ActiveRecord::Base
+  has_many :teams
+  has_many :players, through: :teams
+  has_secure_password
+
+
+  def slug
+    self.username.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find do |user|
+      user.slug == slug
+    end
+  end 
+
 end
--- soccer/app/models/team.rb
+++ soccer/app/models/team.rb
@@ -1,2 +1,4 @@
 class Team < ActiveRecord::Base
+  belongs_to :owner
+  has_many :players
 end
